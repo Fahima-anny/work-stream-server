51d0bfcc4df897e0be75e9730ce0975e9bf4f427
Fahima-anny <fahima.info03@gmail.com>
2025-01-18 07:58:30 +0600

    hr routes done

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express") ;
+const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
-const app = express() ;
+const app = express();
 const cors = require('cors')
-require('dotenv').config() ;
-const port = process.env.PORT || 5000 ;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-app.use(cors()) ;
-app.use(express.json()) ;
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.VITE_MONGO_USER}:${process.env.VITE_MONGO_PASS}@cluster0.ohdc4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,193 +26,250 @@ async function run() {
 
     const userCollection = client.db("workStreamDB").collection("users");
     const workSheetCollection = client.db("workStreamDB").collection("work-sheet");
+    const payrollCollection = client.db("workStreamDB").collection("payroll");
 
-// all verify api 
-// verify Token Middleware 
-const verifyToken = (req, res, next) => {
-  // console.log("inside verify token",req.headers.authorization);
-  if(!req.headers.authorization){
-   return res.status(401).send({message: "Unauthorized Access"}) ;
-  }
-  const token = req.headers.authorization.split(" ")[1] ;
-jwt.verify(token, process.env.VITE_SECRET_KEY, (err, decoded) => {
-  if(err){
-    return res.status(401).send({message: "Unauthorized Access"}) ;
-  }
-  req.decoded = decoded ;
-  // console.log("decoded",decoded.role);
-  next() ;
-})
-}
+    // all verify api 
+    // verify Token Middleware 
+    const verifyToken = (req, res, next) => {
+      // console.log("inside verify token",req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.VITE_SECRET_KEY, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized Access" });
+        }
+        req.decoded = decoded;
+        // console.log("decoded",decoded.role);
+        next();
+      })
+    }
 
-const verifyAdmin = (req, res, next) => {
-  const role = req.decoded.role ;
-  if(role !== "admin"){
-    return res.status(403).send({message: "Forbidden Access"})
-  }
-  next()
-}
+    const verifyAdmin = (req, res, next) => {
+      const role = req.decoded.role;
+      if (role !== "admin") {
+        return res.status(403).send({ message: "Forbidden Access" })
+      }
+      next()
+    }
 
-const verifyHR = (req, res, next) => {
-  const role = req.decoded.role ;
-  if(role !== "HR"){
-    return res.status(403).send({message: "Forbidden Access"})
-  }
-  next()
-}
+    const verifyHR = (req, res, next) => {
+      const role = req.decoded.role;
+      if (role !== "HR") {
+        return res.status(403).send({ message: "Forbidden Access" })
+      }
+      next()
+    }
 
-const verifyEmployee = (req, res, next) => {
-  const role = req.decoded.role ;
-  console.log("from verify employee", role);
-  const checkEmployee = role === "Employee"
-  if(!checkEmployee){
-    console.log("vul manus");
-    return res.status(403).send({message: "Forbidden Access"})
-  }
-  next()
-}
+    const verifyEmployee = (req, res, next) => {
+      const role = req.decoded.role;
+      console.log("from verify employee", role);
+      const checkEmployee = role === "Employee"
+      if (!checkEmployee) {
+        console.log("vul manus");
+        return res.status(403).send({ message: "Forbidden Access" })
+      }
+      next()
+    }
 
 
     // jwt api 
     app.post("/jwt", async (req, res) => {
-      const user = req.body ;
-      const token = jwt.sign(user, process.env.VITE_SECRET_KEY, {expiresIn: '5h'}) ;
-      res.send({token}) ;
+      const user = req.body;
+      const token = jwt.sign(user, process.env.VITE_SECRET_KEY, { expiresIn: '5h' });
+      res.send({ token });
+    })
+
+
+    app.post("/payroll", verifyToken, verifyHR, async (req, res) => {
+      const payrollData = req.body;
+      console.log(payrollData)
+      const { email, month, year } = payrollData;
+
+      const existingPayroll = await payrollCollection.findOne({ email, month, year });
+      console.log(existingPayroll);
+
+      if (existingPayroll) {
+        return res.send({
+          message: "Payment for this employee has already been requested for the same month and year.",
+        });
+      }
+
+      const result = await payrollCollection.insertOne(payrollData);
+      res.send(result);
     })
 
 
     // work sheet related api 
-app.post("/work-sheet", verifyToken, verifyEmployee, async(req, res) => {
-  const workSheetData = req.body ;
-  // console.log(workSheetData);
-  const result = await workSheetCollection.insertOne(workSheetData) ;
-  res.send(result)
-})
+    app.post("/work-sheet", verifyToken, verifyEmployee, async (req, res) => {
+      const workSheetData = req.body;
+      // console.log(workSheetData);
+      const result = await workSheetCollection.insertOne(workSheetData);
+      res.send(result)
+    })
 
-// app.get("/work-sheet/:id", async(req, res) => {
-//   const workId = req.params.id ;
-//   // console.log(workSheetData);
-//   const query = {_id : new ObjectId(workId)}
-//   const result = await workSheetCollection.findOne(query) ;
-//   res.send(result)
-// })
+    app.get("/work-sheet", async (req, res) => {
 
-app.patch("/work-sheet/:id", verifyToken, verifyEmployee, async(req, res) => {
-  const id = req.params.id ;
-  const updatedData = req.body ;
-  console.log(id);
-  const query = {_id: new ObjectId(id)} ;
-  const updateDoc = {
-    $set : {
-      task: updatedData.task,
-      date: updatedData.date,
-      hoursWorked: updatedData.hoursWorked,
-    }
-  }
-  const result = await workSheetCollection.updateOne(query, updateDoc) ;
-  res.send(result) ;
-})
+      const result = await workSheetCollection.aggregate([
+        {
+          $lookup: {
+            from: "users",
+            localField: "email",
+            foreignField: "email",
+            as: "userInfo"
+          },
+        },
+        {
+          $unwind: "$userInfo"
+        },
+        {
+          $project: {
+            _id: 1,
+            email: 1,
+            task: 1,
+            date: 1 ,
+            hoursWorked: 1,
+            userName: "$userInfo.name"
+          },
+        },
+      ]).toArray();
+      res.send(result)
+    })
 
-app.delete("/work-sheet/:id", verifyToken, verifyEmployee , async(req, res) => {
-  const workId = req.params.id ;
-  console.log(workId);
-  const query = {_id : new ObjectId(workId)} ;
-  const result = await workSheetCollection.deleteOne(query) ;
-  res.send(result) ;
-})
+    app.patch("/work-sheet/:id", verifyToken, verifyEmployee, async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          task: updatedData.task,
+          date: updatedData.date,
+          hoursWorked: updatedData.hoursWorked,
+        }
+      }
+      const result = await workSheetCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
+    app.delete("/work-sheet/:id", verifyToken, verifyEmployee, async (req, res) => {
+      const workId = req.params.id;
+      console.log(workId);
+      const query = { _id: new ObjectId(workId) };
+      const result = await workSheetCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
-app.get("/work-sheet/:email", verifyToken, async(req, res) => {
-  const email = req.params.email ;
-  const query = {email: email} ;
-  console.log(email);
-  const result = await workSheetCollection.find(query).toArray() ;
-  res.send(result)
-})
+    app.get("/work-sheet/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      // console.log(email);
+      const result = await workSheetCollection.find(query).toArray();
+      res.send(result)
+    })
 
 
 
     // users api 
-app.post("/users", async(req, res) => {
-    const userInfo = req.body ;
-    // console.log(userInfo);
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      // console.log(userInfo);
 
-    // if user already exits dont insert user 
-    const query = {email: userInfo.email} ;
-    const userExits = await userCollection.findOne(query) ;
-    if(userExits){
-      return res.send({message: "user already exists", insertedId: null})
-    }
+      // if user already exits dont insert user 
+      const query = { email: userInfo.email };
+      const userExits = await userCollection.findOne(query);
+      if (userExits) {
+        return res.send({ message: "user already exists", insertedId: null })
+      }
 
-    // if not insert user 
-    const result = await userCollection.insertOne(userInfo) ;
-    res.send(result) ;
-})
+      // if not insert user 
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    })
 
-app.put("/users/:email", async(req, res) => {
-const updatedInfo = req.body ;
-const filter = {email: req.params.email} ;
-const options = { upsert: true } ;
-const updateDoc = {
-  $set: {
-   salary: updatedInfo.salary,
-   image: updatedInfo.image,
-   bankAcc: updatedInfo.bankAcc,
-   designation: updatedInfo.designation,
-   role: "Employee",
-   isVerified: false,
-   isActive: true,
-  },
-};
-const result = await userCollection.updateOne(filter,updateDoc,options) ;
-res.send(result) ;
-})
+    app.patch("/users/:id", async (req, res) => {
+      const status = req.body.status;
+      const id = req.params.id;
+      // console.log(id,status);
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          isVerified: status
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    })
 
-app.get("/users/:email", async(req, res) => {
-  const email = req.params.email ;
-  // console.log(email);
-  const query = {email: email} ;
-  const user = await userCollection.findOne(query) ;
-  // console.log(user.role);
-res.send(user?.role) ;
-})
+    app.put("/users/:email", async (req, res) => {
+      const updatedInfo = req.body;
+      const filter = { email: req.params.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          salary: updatedInfo.salary,
+          image: updatedInfo.image,
+          bankAcc: updatedInfo.bankAcc,
+          designation: updatedInfo.designation,
+          role: "Employee",
+          isVerified: false,
+          isActive: true,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
 
-
-app.get("/users/role/:email", verifyToken, async(req, res) => {
-
-    const email = req.params.email ;
-    const decodedEmail = req.decoded.email ;
-    const role = req.decoded.role ;
-    console.log(email,role);
-
-    if(email !== decodedEmail){
-      return res.status(403).send({message: "Forbidden Access"})
-    }
-
-    if(role === 'admin'){
-       return res.send('admin') ;
-    }
-
-    if(role === 'HR'){
-       return res.send('HR') ;
-    }
-    if(role === "Employee"){
-      return res.send('Employee')
-    }
-
-})
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email);
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      // console.log(user.role);
+      res.send(user?.role);
+    })
 
 
-// app.get("/users/hr/:email", async(req, res) => {
-//     const email = req.params.email ;
-//     const query = {email: email} ;
-//     const user = await userCollection.findOne(query) ;
-//     let hr = false ;
-//     if(user){
-//         hr = user?.role === "HR" ;
-//     }
-//     res.send({hr}) ;
-// })
+    app.get("/users/role/:email", verifyToken, async (req, res) => {
+
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+      const role = req.decoded.role;
+      // console.log(email,role);
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "Forbidden Access" })
+      }
+
+      if (role === 'admin') {
+        return res.send('admin');
+      }
+
+      if (role === 'HR') {
+        return res.send('HR');
+      }
+      if (role === "Employee") {
+        return res.send('Employee')
+      }
+
+    })
+
+
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      const result = users.filter(u => u.role === "Employee")
+      res.send(result);
+    })
+
+    app.get("/users/id/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(req);
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
 
 
 
@@ -231,9 +288,9 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("My Server is running bro") ;
-}) ;
+  res.send("My Server is running bro");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 })
