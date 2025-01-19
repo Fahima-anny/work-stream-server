c5c5e1f5626cf1eec11b684b2b9a13b0d9095c11
Fahima-anny <fahima.info03@gmail.com>
2025-01-20 04:52:06 +0600

    payment history done

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express();
-const cors = require('cors')
-require('dotenv').config();
+const cors = require('cors');
+const stripe = require("stripe")(process.env.VITE_STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -64,10 +65,10 @@ async function run() {
 
     const verifyEmployee = (req, res, next) => {
       const role = req.decoded.role;
-      console.log("from verify employee", role);
+      // console.log("from verify employee", role);
       const checkEmployee = role === "Employee"
       if (!checkEmployee) {
-        console.log("vul manus");
+        // console.log("vul manus");
         return res.status(403).send({ message: "Forbidden Access" })
       }
       next()
@@ -84,22 +85,102 @@ async function run() {
 
     app.post("/payroll", verifyToken, verifyHR, async (req, res) => {
       const payrollData = req.body;
-      console.log(payrollData)
+      // console.log(payrollData)
       const { email, month, year } = payrollData;
 
       const existingPayroll = await payrollCollection.findOne({ email, month, year });
-      console.log(existingPayroll);
+      // console.log(existingPayroll);
 
       if (existingPayroll) {
         return res.send({
           message: "Payment for this employee has already been requested for the same month and year.",
         });
       }
-
       const result = await payrollCollection.insertOne(payrollData);
       res.send(result);
     })
 
+
+    app.get("/payroll", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await payrollCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get("/payroll/:email", verifyToken, verifyEmployee, async (req, res) => {
+      const email = req.params.email;
+      // console.log(req.query);
+
+      const page = parseInt(req.query.page) || 1;
+      const size = parseInt(req.query.size) || 5;
+
+      const query = { email: email, transactionId: { $exists: true, $ne: null } };
+      const totalCount = await payrollCollection.countDocuments(query);
+
+      const ddd = await payrollCollection.find(query).sort({ year: -1, month: -1 }).toArray()
+      // console.log(ddd);
+
+      const result = await payrollCollection
+        .aggregate([
+          // Match the query
+          { $match: query },
+          // Add fields to ensure year and month are treated as numbers
+          {
+            $addFields: {
+              yearNumeric: { $toInt: "$year" },
+              monthNumeric: { $toInt: "$month" },
+            },
+          },
+          // Sort by year and month in descending order
+          { $sort: { yearNumeric: -1, monthNumeric: -1 } },
+          // Skip and limit for pagination
+          { $skip: (page - 1) * size },
+          { $limit: size },
+        ])
+        .toArray();
+
+      res.send({
+        data: result, // Paginated and filtered data
+        totalCount, // Total number of filtered records
+        currentPage: page, // Current page number
+        pageSize: size, // Items per page
+      });
+    })
+
+    app.get("/payroll/bar/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await payrollCollection.aggregate([
+        // Match the query
+        { $match: query },
+        // Add fields to ensure year and month are treated as numbers
+        {
+          $addFields: {
+            yearNumeric: { $toInt: "$year" },
+            monthNumeric: { $toInt: "$month" },
+          },
+        },
+        // Sort by year and month in ascending order
+        { $sort: { yearNumeric: 1, monthNumeric: 1 } },
+      ]).toArray()
+      console.log("bar chart", result);
+      res.send(result)
+    })
+
+    app.patch("/payroll/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const updatedData = req.body;
+      // console.log(updatedData);
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      Option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          transactionId: updatedData.transactionId,
+          paymentDate: updatedData.paymentDate
+        }
+      }
+      const result = await payrollCollection.updateOne(query, updatedDoc, Option);
+      res.send(result);
+    })
 
     // work sheet related api 
     app.post("/work-sheet", verifyToken, verifyEmployee, async (req, res) => {
@@ -128,7 +209,7 @@ async function run() {
             _id: 1,
             email: 1,
             task: 1,
-            date: 1 ,
+            date: 1,
             hoursWorked: 1,
             userName: "$userInfo.name"
           },
@@ -140,7 +221,7 @@ async function run() {
     app.patch("/work-sheet/:id", verifyToken, verifyEmployee, async (req, res) => {
       const id = req.params.id;
       const updatedData = req.body;
-      console.log(id);
+      // console.log(id);
       const query = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
@@ -155,7 +236,7 @@ async function run() {
 
     app.delete("/work-sheet/:id", verifyToken, verifyEmployee, async (req, res) => {
       const workId = req.params.id;
-      console.log(workId);
+      // console.log(workId);
       const query = { _id: new ObjectId(workId) };
       const result = await workSheetCollection.deleteOne(query);
       res.send(result);
@@ -189,7 +270,13 @@ async function run() {
       res.send(result);
     })
 
-    app.patch("/users/:id", async (req, res) => {
+    app.get("/users/verified", verifyToken, verifyAdmin, async (req, res) => {
+      const query = { isVerified: true }
+      const users = await userCollection.find(query).toArray();
+      res.send(users);
+    })
+
+    app.patch("/users/:id", verifyToken, verifyHR, async (req, res) => {
       const status = req.body.status;
       const id = req.params.id;
       // console.log(id,status);
@@ -200,6 +287,52 @@ async function run() {
         }
       }
       const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    })
+
+    app.patch("/users/fire/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          isActive: false
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    })
+
+    app.patch("/users/make-admin/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "HR"
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    })
+
+    app.patch("/users/promotion/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          salary: updatedData.salary
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    })
+
+    app.get("/users/fired", async (req, res) => {
+      const email = req.query.email;
+      // console.log(email);
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      // console.log(result);
       res.send(result);
     })
 
@@ -265,12 +398,29 @@ async function run() {
 
     app.get("/users/id/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(req);
+      // console.log(req);
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.findOne(query);
       res.send(result);
     })
 
+
+    // payment api 
+    app.post("/create-payment-intent", verifyToken, verifyAdmin, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      // console.log("amount = ", amount);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ["card"],
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
 
 
     // Connect the client to the server	(optional starting in v4.7)
